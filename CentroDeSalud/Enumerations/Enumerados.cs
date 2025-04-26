1fce7befdaee8a4eb2666df19bbc93dc5ec655ca
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-04-26 12:50:00 +0200

    🔄 Cambios en los numeros de los enumerado para el tema de orden y adición de "NoSeleccionado" a todos estos.

--- CentroDeSalud/Enumerations/Enumerados.cs
+++ CentroDeSalud/Enumerations/Enumerados.cs
@@ -4,35 +4,40 @@ namespace CentroDeSalud.Enumerations
 {
     public enum Sexo
     {
-        Hombre = 0,
-        Mujer = 1,
-        Otro = 2
+        [Display(Name = "Seleccione un sexo")]
+        NoSeleccionado = 0,
+        Hombre = 1,
+        Mujer = 2,
+        Otro = 3
     }
 
     public enum GrupoSanguineo
     {
+        [Display(Name = "Seleccione un grupo sanguíneo")]
+        NoSeleccionado = 0,
+
         [Display(Name = "A+")]
-        Apositivo = 0,
+        Apositivo = 1,
 
         [Display(Name = "A-")]
-        Anegativo = 1,
+        Anegativo = 2,
 
         [Display(Name = "B+")]
-        Bpositivo = 2,
+        Bpositivo = 3,
 
         [Display(Name = "B-")]
-        Bnegativo = 3,
+        Bnegativo = 4,
 
         [Display(Name = "AB+")]
-        ABpositivo = 4,
+        ABpositivo = 5,
 
         [Display(Name = "AB-")]
-        ABnegativo = 5,
+        ABnegativo = 6,
 
         [Display(Name = "O+")]
-        Opositivo = 6,
+        Opositivo = 7,
 
         [Display(Name = "O-")]
-        Onegativo = 7
+        Onegativo = 8
     }
 }
