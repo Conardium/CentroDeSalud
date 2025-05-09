f75808900ba0868c98fd1a1b8ae7e69a31737a57
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-05-09 13:45:14 +0200

    ViewModel que contiene los datos del Aviso a mostrar al usuario y valores constantes para comprobar el tipo de mensaje a mostrar

--- CentroDeSalud/Data/Constantes.cs
+++ CentroDeSalud/Data/Constantes.cs
@@ -5,5 +5,9 @@
         public const string RolAdmin = "Admin";
         public const string RolMedico = "Medico";
         public const string RolPaciente = "Paciente";
+
+        public const string OK = "OK";
+        public const string Error = "Error";
+        public const string Denegado = "Denegado";
     }
 }
