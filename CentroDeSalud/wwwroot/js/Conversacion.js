e13af388d26ba9e5947b0db420b8def17edb147c
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-05-13 18:52:45 +0200

    Se añadieron estilos nuevos para ciertas páginas, una imagen nueva para el chat y la importación de la biblioteca del lado del cliente de signalr con libman

---
+++ CentroDeSalud/wwwroot/js/Conversacion.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('input', function (event) {
+    if (event.target.classList.contains('auto-grow')) {
+        event.target.style.height = 'auto';
+        event.target.style.height = (event.target.scrollHeight) + 'px';
+    }
+});
