a70b5cbc24670455d075b92304ba596046a54610
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-05-19 12:22:18 +0200

    Se ha mejorado la forma de enviar los avisos al controlador de avisos para usar menos codigo repetitivo

--- CentroDeSalud/Controllers/AvisosController.cs
+++ CentroDeSalud/Controllers/AvisosController.cs
@@ -1,4 +1,5 @@
-﻿using CentroDeSalud.Models.ViewModels;
+﻿using CentroDeSalud.Data;
+using CentroDeSalud.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,13 +8,54 @@ namespace CentroDeSalud.Controllers
     public class AvisosController : Controller
     {
         [AllowAnonymous]
-        public IActionResult AvisosGenerales(AvisoViewModel modelo)
+        public IActionResult Exito(string mensaje)
         {
             if (!TempData.ContainsKey("Acceso"))
                 return NotFound();
 
+            var aviso = new AvisoViewModel
+            {
+                Titulo = "Exito",
+                Tipo = Constantes.OK,
+                Mensaje = mensaje
+            };
+
             TempData.Remove("Acceso");
-            return View(modelo);
+            return View("AvisosGenerales", aviso);
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error(string mensaje)
+        {
+            if (!TempData.ContainsKey("Acceso"))
+                return NotFound();
+
+            var aviso = new AvisoViewModel
+            {
+                Titulo = "Error",
+                Tipo = Constantes.Error,
+                Mensaje = mensaje
+            };
+
+            TempData.Remove("Acceso");
+            return View("AvisosGenerales", aviso);
+        }
+
+        [AllowAnonymous]
+        public IActionResult Denegado(string mensaje)
+        {
+            if (!TempData.ContainsKey("Acceso"))
+                return NotFound();
+
+            var aviso = new AvisoViewModel
+            {
+                Titulo = "Acceso denegado",
+                Tipo = Constantes.Denegado,
+                Mensaje = "No tienes permisos para acceder a esta página de la web."
+            };
+
+            TempData.Remove("Acceso");
+            return View("AvisosGenerales", aviso);
         }
     }
 }
