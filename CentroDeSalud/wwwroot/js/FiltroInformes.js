d5125968f4d877fbe5a2d8f6d7cd101921685a23
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-05-23 13:22:10 +0200

    🌟 HU 21 - Modificar Informes : Completada

--- CentroDeSalud/wwwroot/js/FiltroInformes.js
+++ CentroDeSalud/wwwroot/js/FiltroInformes.js
@@ -9,17 +9,22 @@
         return new Date(`${anio}-${mes}-${dia}`);
     }
 
+    function normalizarTexto(texto) {
+        return texto.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+
     function filtrar() {
         const inicio = fechaInicio.value ? new Date(fechaInicio.value) : null;
         const fin = fechaFin.value ? new Date(fechaFin.value) : null;
-        const estado = estadoFiltro.value;
+        const estado = normalizarTexto(estadoFiltro.value);
+        debugger;
 
         filas.forEach(fila => {
             const fechaTexto = fila.cells[1].textContent.trim();
             const fechaInforme = formatearFecha(fechaTexto);
 
-            const estadoTexto = fila.cells[fila.cells.length - 2].textContent.trim();
-
+            const estadoTexto = normalizarTexto(fila.cells[fila.cells.length - 2].textContent.trim()).replace(/\s+/g, '');
+            debugger;
             let visible = true;
 
             if (inicio && fechaInforme < inicio) visible = false;
