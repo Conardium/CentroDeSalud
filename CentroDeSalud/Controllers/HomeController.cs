10aa96ff86c10da3d6ca36eba9d7dd8430409dc2
Ismael Da Palma Fernández <ismaeldapalmaf@gmail.com>
2025-06-01 18:48:25 +0200

    Creación inicial de la página de inicio

--- CentroDeSalud/Controllers/HomeController.cs
+++ CentroDeSalud/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CentroDeSalud.Models.ViewModels;
+using CentroDeSalud.Services;
 
 namespace CentroDeSalud.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IServicioPublicaciones servicioPublicaciones;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IServicioPublicaciones servicioPublicaciones)
     {
         _logger = logger;
+        this.servicioPublicaciones = servicioPublicaciones;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var modelo = new PaginaInicioViewModel();
+
+        modelo.Destacadas = (ICollection<Models.Publicacion>)
+            await servicioPublicaciones.ListarDestacadas();
+        modelo.Publicaciones = (ICollection<Models.Publicacion>)
+            await servicioPublicaciones.ListarUltimasNoDestacadas(4);
+
+        return View(modelo);
     }
 
     public IActionResult Privacy()
